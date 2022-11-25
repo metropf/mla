438460d9986baad224f93f6c5c0b1311d1fafbc8
metropf <106774621+metropf@users.noreply.github.com>
2022-11-25 16:05:35 +0200

    Forgot to remove references to removed types

--- include/MLA/mla.h
+++ include/MLA/mla.h
@@ -4,28 +4,22 @@
 #include "util.h"
 
 #include "vec2i.h"
-#include "vec2u.h"
 #include "vec2f.h"
 
 #include "vec3i.h"
-#include "vec3u.h"
 #include "vec3f.h"
 
 #include "vec4i.h"
-#include "vec4u.h"
 #include "vec4f.h"
 
 
 #include "mat2i.h"
-#include "mat2u.h"
 #include "mat2f.h"
 
 #include "mat3i.h"
-#include "mat3u.h"
 #include "mat3f.h"
 
 #include "mat4i.h"
-#include "mat4u.h"
 #include "mat4f.h"
 
 #endif // _mla_h_
--- test/src/main.c
+++ test/src/main.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-	mat3u matrix = mla_mat3u_init();
-	matrix = mla_mat3u_mul(&matrix, &matrix);
+	mat3i matrix = mla_mat3i_init();
+	matrix = mla_mat3i_mul(&matrix, &matrix);
 
-	uint32_t matrix_arr[3][3];
-	mla_mat3u_array(&matrix, matrix_arr);
+	int32_t matrix_arr[3][3];
+	mla_mat3i_array(&matrix, matrix_arr);
 
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < 3; ++j)
